1065dc17f39883e40f524284927ab34df0599ff8
Abhith <abhith@live.in>
2015-07-14 15:22:58 +0530

    File helper added

---
+++ Source/Code.Library/Code.Library/FileHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace Code.Library
+{
+    public static class FileHelper
+    {
+        /// <summary>
+        /// Delete file
+        /// Author : Abhith
+        /// Date : 14 July 2015
+        /// Reference : Sysberries
+        /// </summary>
+        /// <param name="fileName"></param>
+        public static void DeleteFile(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return;
+            }
+
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+        }
+    }
+}
