d6b979d54e95d031ebff5d686b697ab45d8ba063
Abhith <abhith@live.in>
2015-07-14 10:39:56 +0530

    String helper class added

---
+++ Source/Code.Library/Code.Library/StringHelper.cs
@@ -0,0 +1,37 @@
+﻿namespace Code.Library
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Author : Abhith
+        /// Date : 14 July 2015
+        /// Uppercase words. This program defines a method named UppercaseWords that is equivalent to the ucwords function in scripting languages such as PHP. The UppercaseWords method internally converts the string to a character array buffer.
+        /// Source : http://www.dotnetperls.com/uppercase-first-letter
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string UppercaseWords(string value)
+        {
+            var array = value.ToCharArray();
+            // Handle the first letter in the string.
+            if (array.Length >= 1)
+            {
+                if (char.IsLower(array[0]))
+                {
+                    array[0] = char.ToUpper(array[0]);
+                }
+            }
+            // Scan through the letters, checking for spaces.
+            // ... Uppercase the lowercase letters following spaces.
+            for (var i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] != ' ') continue;
+                if (char.IsLower(array[i]))
+                {
+                    array[i] = char.ToUpper(array[i]);
+                }
+            }
+            return new string(array);
+        }
+    }
+}
