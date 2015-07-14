6b4377160fdc91a0806596da4fc918e310d1034c
Abhith <abhith@live.in>
2015-07-14 15:20:29 +0530

    resize img added

--- Source/Code.Library/Code.Library/ImageHelper.cs
+++ Source/Code.Library/Code.Library/ImageHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
@@ -78,6 +79,34 @@ namespace Code.Library
             }
 
             return ImageFormat.Png;
+        }
+
+        /// <summary>
+        /// Resize Image
+        /// Author : Abhith
+        /// Date : 14 July 2015
+        /// Reference : Sysberries
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="maxWidth"></param>
+        /// <param name="maxHeight"></param>
+        /// <returns></returns>
+        public static Bitmap ResizeImage(Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            Graphics.FromImage(newImage).DrawImage(image, 0, 0, newWidth, newHeight);
+            Bitmap bmp = new Bitmap(newImage);
+
+            return bmp;
         }
     }
 }
