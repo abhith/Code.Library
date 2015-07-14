beb9fc249df669138d7dfd4214cdef33a1a784fd
Abhith <abhith@live.in>
2015-07-14 15:30:10 +0530

    Email helper class with sendgrid code added

---
+++ Source/Code.Library/Code.Library/EmailHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace Code.Library
+{
+    public static class EmailHelper
+    {
+        //public static void SendgridEmail(string destination, string subject, string body)
+        //{
+        //    var myMessage = new SendGridMessage();
+        //    myMessage.AddTo(destination);
+        //    myMessage.From = new System.Net.Mail.MailAddress("Email-From", "Email-Name");
+        //    myMessage.Subject = subject;
+        //    myMessage.Text = body;
+        //    myMessage.Html = body;
+
+        //    //Template
+        //    //switch (subject)
+        //    //{
+        //    //    case "\":
+        //    //        myMessage.EnableTemplateEngine(WebConfigurationManager.AppSettings["templateId"]);
+        //    //        break;    
+        //    //}
+
+        //    var credentials = new NetworkCredential("Sendgrid Username","Sendgrid Password");
+
+        //    // Create a Web transport for sending email. 
+        //    var transportWeb = new Web(credentials);
+
+        //    // Send the email. 
+        //    transportWeb.DeliverAsync(myMessage);
+        //}
+    }
+}
