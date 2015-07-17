d17d50e77402001ba2865a3a646607618db147e8
JithinPS <jsjithin427@gmail.com>
2015-07-17 16:11:57 +0530

    Convert helper added

---
+++ Source/Code.Library/Code.Library/ConvertHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Library
+{
+    public static class ConvertHelper
+    {
+
+        // funtion for making a datatable from the template data
+        public static DataTable ToDataTable<T>(IEnumerable<T> data)
+        {
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+            var table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+    }
+}
