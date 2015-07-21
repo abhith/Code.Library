ac6e29752023bad1cabe21c3faecc4f80181831b
Abhith <dearabhith@gmail.com>
2015-07-21 12:19:15 +0530

    qr code image url function added to thirdparty helper

--- Source/Code.Library/Code.Library/ThirdPartyHelper.cs
+++ Source/Code.Library/Code.Library/ThirdPartyHelper.cs
@@ -19,5 +19,15 @@ namespace Code.Library
         {
             return string.Format("https://www.google.com/calendar/render?action=TEMPLATE&text={0}&dates={1}Z/{2}Z&details=For+details,+link+here:{3}&location={4}&sf=true&output=xml", title, startDate.ToString("yyyyMMdd'T'HHmmss"), endDate.ToString("yyyyMMdd'T'HHmmss"),detailsUrl,location);
         }
+
+        /// <summary>
+        /// Get image url for the qr code
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string GetQrCodeImageUrl(string url)
+        {
+            return string.Format("http://chart.apis.google.com/chart?cht=qr&chs=400x400&chl={0}", url);
+        }
     }
 }
