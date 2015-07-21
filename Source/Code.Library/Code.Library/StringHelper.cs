b79bfe77d18994e81b7a8420d959ef63ff9628d6
Abhith <dearabhith@gmail.com>
2015-07-21 11:31:01 +0530

    The regex strip html added to string helper

--- Source/Code.Library/Code.Library/StringHelper.cs
+++ Source/Code.Library/Code.Library/StringHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Code.Library
@@ -103,6 +105,52 @@ namespace Code.Library
             // remove trailing dashes
             title = title.TrimEnd(new[] { '-' });
             return title;
+        }
+
+        /// <summary>
+        /// The regex strip html.
+        /// Author : Abhith
+        /// Date 21 July 2015
+        /// Reference : Gist
+        /// </summary>
+        private static readonly Regex RegexStripHtml = new Regex("<[^>]*>", RegexOptions.Compiled);
+
+        private static string StripHtml(string html)
+        {
+            return string.IsNullOrWhiteSpace(html) ? string.Empty :
+                RegexStripHtml.Replace(html, string.Empty).Trim();
+        }
+
+        public static string CleanContent(string content, bool removeHtml)
+        {
+            if (removeHtml)
+            {
+                content = StripHtml(content);
+            }
+
+            content =
+                content.Replace("\\", string.Empty).
+                Replace("|", string.Empty).
+                Replace("(", string.Empty).
+                Replace(")", string.Empty).
+                Replace("[", string.Empty).
+                Replace("]", string.Empty).
+                Replace("*", string.Empty).
+                Replace("?", string.Empty).
+                Replace("}", string.Empty).
+                Replace("{", string.Empty).
+                Replace("^", string.Empty).
+                Replace("+", string.Empty);
+
+            var words = content.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            var sb = new StringBuilder();
+            foreach (var word in
+                words.Select(t => t.ToLowerInvariant().Trim()).Where(word => word.Length > 1))
+            {
+                sb.AppendFormat("{0} ", word);
+            }
+
+            return sb.ToString();
         }
     }
 }
