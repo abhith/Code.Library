3ca5cb9887c1c55d68b280294d9400b5953b3b29
Abhith <dearabhith@gmail.com>
2015-08-06 13:22:36 +0530

    Debug helper added  #6

---
+++ Source/Code.Library/Code.Library/DebugHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+
+namespace Code.Library
+{
+    public static class DebugHelper
+    {
+        /// <summary>
+        /// Log message to the response header
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="message"></param>
+        public static void LogMessage(string title,string message)
+        {
+            HttpContext.Current.Response.Write(string.Format("| {0} | {1} - {2}",DateTime.Now,title,message));
+        }
+    }
+}
