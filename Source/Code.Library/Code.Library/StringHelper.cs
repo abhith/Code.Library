574d3989170e69c44de980bcec6e85b78c5aa7ba
Abhith <dearabhith@gmail.com>
2015-08-14 00:46:40 +0530

    #7 more functions from found article added

--- Source/Code.Library/Code.Library/StringHelper.cs
+++ Source/Code.Library/Code.Library/StringHelper.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace Code.Library
 {
     public static class StringHelper
     {
         /// <summary>
-        /// Author : Abhith
-        /// Date : 14 July 2015
         /// Uppercase words. This program defines a method named UppercaseWords that is equivalent to the ucwords function in scripting languages such as PHP. The UppercaseWords method internally converts the string to a character array buffer.
         /// Source : http://www.dotnetperls.com/uppercase-first-letter
         /// </summary>
@@ -41,8 +43,6 @@ namespace Code.Library
 
         /// <summary>
         /// Format Dates
-        /// Author : Abhith
-        /// Created On : 24 June 2015
         /// </summary>
         /// <param name="startDate"></param>
         /// <param name="endDate"></param>
@@ -109,8 +109,6 @@ namespace Code.Library
 
         /// <summary>
         /// The regex strip html.
-        /// Author : Abhith
-        /// Date 21 July 2015
         /// Reference : Gist
         /// </summary>
         private static readonly Regex RegexStripHtml = new Regex("<[^>]*>", RegexOptions.Compiled);
@@ -155,8 +153,6 @@ namespace Code.Library
 
         /// <summary>
         /// Truncate string at a word near to the limit specified. Avoid word split.
-        /// Author : Abhith
-        /// Date : 21 July 2015
         /// </summary>
         /// <param name="input"></param>
         /// <param name="length"></param>
@@ -175,5 +171,236 @@ namespace Code.Library
             }
             return trimmedInput;
         }
+
+        /// <summary>
+        /// true, if is valid email address
+        /// from http://www.codeproject.com/Articles/31050/String-Extension-Collection-for-C
+        /// </summary>
+        /// <param name="s">email address to test</param>
+        /// <returns>true, if is valid email address</returns>
+
+        public static bool IsValidEmailAddress(this string s)
+        {
+            return new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$").IsMatch(s);
+        }
+
+        /// <summary>
+        /// Checks if url is valid. 
+        /// from http://www.osix.net/modules/article/?id=586
+        /// and changed to match http://localhost
+        /// 
+        /// complete (not only http) url regex can be found 
+        /// at http://internet.ls-la.net/folklore/url-regexpr.html
+        /// </summary>
+        /// <param name="text"></param>
+
+        /// <returns></returns>
+        public static bool IsValidUrl(this string url)
+        {
+            string strRegex = "^(https?://)"
+        + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //user@
+        + @"(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP- 199.194.52.184
+        + "|" // allows either IP or domain
+        + @"([0-9a-z_!~*'()-]+\.)*" // tertiary domain(s)- www.
+        + @"([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]" // second level domain
+        + @"(\.[a-z]{2,6})?)" // first level domain- .com or .museum is optional
+        + "(:[0-9]{1,5})?" // port number- :80
+        + "((/?)|" // a slash isn't required if there is no file name
+        + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+            return new Regex(strRegex).IsMatch(url);
+        }
+
+
+
+        /// <summary>
+        /// Check if url (http) is available.
+        /// </summary>
+        /// <param name="httpUri">url to check</param>
+        /// <example>
+
+        /// string url = "www.codeproject.com;
+        /// if( !url.UrlAvailable())
+        ///     ...codeproject is not available
+        /// </example>
+        /// <returns>true if available</returns>
+        public static bool UrlAvailable(this string httpUrl)
+        {
+            if (!httpUrl.StartsWith("http://") || !httpUrl.StartsWith("https://"))
+                httpUrl = "http://" + httpUrl;
+            try
+            {
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(httpUrl);
+                myRequest.Method = "GET";
+                myRequest.ContentType = "application/x-www-form-urlencoded";
+                HttpWebResponse myHttpWebResponse =
+                   (HttpWebResponse)myRequest.GetResponse();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+
+        /// Reverse the string
+        /// from http://en.wikipedia.org/wiki/Extension_method
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Reverse(this string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new String(chars);
+        }
+
+        /// <summary>
+
+        /// Reduce string to shorter preview which is optionally ended by some string (...).
+        /// </summary>
+        /// <param name="s">string to reduce</param>
+        /// <param name="count">Length of returned string including endings.</param>
+        /// <param name="endings">optional edings of reduced text</param>
+
+        /// <example>
+        /// string description = "This is very long description of something";
+        /// string preview = description.Reduce(20,"...");
+        /// produce -> "This is very long..."
+        /// </example>
+        /// <returns></returns>
+
+        public static string Reduce(this string s, int count, string endings)
+        {
+            if (count < endings.Length)
+                throw new Exception("Failed to reduce to less then endings length.");
+            int sLength = s.Length;
+            int len = sLength;
+            if (endings != null)
+                len += endings.Length;
+            if (count > sLength)
+                return s; //it's too short to reduce
+            s = s.Substring(0, sLength - len + count);
+            if (endings != null)
+                s += endings;
+            return s;
+        }
+
+        /// <summary>
+        /// remove white space, not line end
+        /// Useful when parsing user input such phone,
+        /// price int.Parse("1 000 000".RemoveSpaces(),.....
+        /// </summary>
+        /// <param name="s"></param>
+
+        /// <param name="value">string without spaces</param>
+        public static string RemoveSpaces(this string s)
+        {
+            return s.Replace(" ", "");
+        }
+
+        /// <summary>
+        /// true, if the string can be parse as Double respective Int32
+        /// Spaces are not considred.
+        /// </summary>
+        /// <param name="s">input string</param>
+
+        /// <param name="floatpoint">true, if Double is considered,
+        /// otherwhise Int32 is considered.</param>
+        /// <returns>true, if the string contains only digits or float-point</returns>
+        public static bool IsNumber(this string s, bool floatpoint)
+        {
+            int i;
+            double d;
+            string withoutWhiteSpace = s.RemoveSpaces();
+            if (floatpoint)
+                return double.TryParse(withoutWhiteSpace, NumberStyles.Any,
+                    Thread.CurrentThread.CurrentUICulture, out d);
+            else
+                return int.TryParse(withoutWhiteSpace, out i);
+        }
+
+        /// <summary>
+        /// true, if the string contains only digits or float-point.
+        /// Spaces are not considred.
+        /// </summary>
+        /// <param name="s">input string</param>
+
+        /// <param name="floatpoint">true, if float-point is considered</param>
+        /// <returns>true, if the string contains only digits or float-point</returns>
+        public static bool IsNumberOnly(this string s, bool floatpoint)
+        {
+            s = s.Trim();
+            if (s.Length == 0)
+                return false;
+            foreach (char c in s)
+            {
+                if (!char.IsDigit(c))
+                {
+                    if (floatpoint && (c == '.' || c == ','))
+                        continue;
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Remove accent from strings 
+        /// </summary>
+        /// <example>
+        ///  input:  "Příliš žluťoučký kůň úpěl ďábelské ódy."
+        ///  result: "Prilis zlutoucky kun upel dabelske ody."
+        /// </example>
+        /// <param name="s"></param>
+        /// <remarks>founded at http://stackoverflow.com/questions/249087/
+        /// how-do-i-remove-diacritics-accents-from-a-string-in-net</remarks>
+        /// <returns>string without accents</returns>
+
+        public static string RemoveDiacritics(this string s)
+        {
+            string stFormD = s.Normalize(NormalizationForm.FormD);
+            StringBuilder sb = new StringBuilder();
+
+            for (int ich = 0; ich < stFormD.Length; ich++)
+            {
+                UnicodeCategory uc = CharUnicodeInfo.GetUnicodeCategory(stFormD[ich]);
+                if (uc != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(stFormD[ich]);
+                }
+            }
+            return (sb.ToString().Normalize(NormalizationForm.FormC));
+        }
+
+        /// <summary>
+        /// Replace \r\n or \n by <br />
+        /// from http://weblogs.asp.net/gunnarpeipman/archive/2007/11/18/c-extension-methods.aspx
+        /// </summary>
+
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string Nl2Br(this string s)
+        {
+            return s.Replace("\r\n", "<br />").Replace("\n", "<br />");
+        }
+
+        /// <summary>
+        static MD5CryptoServiceProvider s_md5 = null;
+
+        /// from http://weblogs.asp.net/gunnarpeipman/archive/2007/11/18/c-extension-methods.aspx
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string MD5(this string s)
+        {
+            if (s_md5 == null) //creating only when needed
+                s_md5 = new MD5CryptoServiceProvider();
+            Byte[] newdata = Encoding.Default.GetBytes(s);
+            Byte[] encrypted = s_md5.ComputeHash(newdata);
+            return BitConverter.ToString(encrypted).Replace("-", "").ToLower();
+        }
+
     }
 }
