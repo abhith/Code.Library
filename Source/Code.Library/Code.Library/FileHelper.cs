42d0e149a72ce99b61e24ef407bc769cf37a8000
Abhith <dearabhith@gmail.com>
2016-01-31 12:13:12 +0400

    create directory added

--- Source/Code.Library/Code.Library/FileHelper.cs
+++ Source/Code.Library/Code.Library/FileHelper.cs
@@ -1,16 +1,27 @@
-﻿using System.IO;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FileHelper.cs" company="Open Source">
+//   File Helper
+// </copyright>
+// <summary>
+//   The file helper.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Code.Library
 {
+    using System.IO;
+
+    /// <summary>
+    /// The file helper.
+    /// </summary>
     public static class FileHelper
     {
         /// <summary>
-        /// Delete file
-        /// Author : Abhith
-        /// Date : 14 July 2015
-        /// Reference : Sysberries
+        /// The delete file.
         /// </summary>
-        /// <param name="fileName"></param>
+        /// <param name="fileName">
+        /// The file name.
+        /// </param>
         public static void DeleteFile(string fileName)
         {
             if (string.IsNullOrEmpty(fileName))
@@ -21,6 +32,21 @@ namespace Code.Library
             if (File.Exists(fileName))
             {
                 File.Delete(fileName);
+            }
+        }
+
+        /// <summary>
+        /// The create directory.
+        /// </summary>
+        /// <param name="path">
+        /// The path.
+        /// </param>
+        public static void CreateDirectory(string path)
+        {
+            var exists = Directory.Exists(System.Web.HttpContext.Current.Server.MapPath(path));
+            if (!exists)
+            {
+                Directory.CreateDirectory(System.Web.HttpContext.Current.Server.MapPath(path));
             }
         }
     }
