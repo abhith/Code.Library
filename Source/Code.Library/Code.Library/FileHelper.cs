96200701c85d06c682daa8c193900b1ea9af9bfc
Abhith <dearabhith@gmail.com>
2016-02-04 12:46:07 +0400

    get file from ftp added

--- Source/Code.Library/Code.Library/FileHelper.cs
+++ Source/Code.Library/Code.Library/FileHelper.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="FileHelper.cs" company="Open Source">
-//   File Helper
+//   Open Source
 // </copyright>
 // <summary>
 //   The file helper.
@@ -10,6 +10,7 @@
 namespace Code.Library
 {
     using System.IO;
+    using System.Net;
 
     /// <summary>
     /// The file helper.
@@ -49,5 +50,59 @@ namespace Code.Library
                 Directory.CreateDirectory(System.Web.HttpContext.Current.Server.MapPath(path));
             }
         }
+
+        #region FTP
+
+        /// <summary>
+        /// The get file from ftp.
+        /// </summary>
+        /// <param name="downloadTo">
+        /// The download to.
+        /// </param>
+        /// <param name="filename">
+        /// The filename.
+        /// </param>
+        /// <param name="ftpAddress">
+        /// The ftp address.
+        /// </param>
+        /// <param name="ftpUsername">
+        /// The ftp username.
+        /// </param>
+        /// <param name="ftpPassword">
+        /// The ftp password.
+        /// </param>
+        public static void GetFileFromFTP(string downloadTo, string filename, string ftpAddress, string ftpUsername, string ftpPassword)
+        {
+            var localPath = downloadTo;
+            var fileName = filename;
+
+            var requestFileDownload = (FtpWebRequest)WebRequest.Create(ftpAddress + fileName);
+            requestFileDownload.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+            requestFileDownload.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            var responseFileDownload = (FtpWebResponse)requestFileDownload.GetResponse();
+
+            var responseStream = responseFileDownload.GetResponseStream();
+            var writeStream = new FileStream(localPath + fileName, FileMode.Create);
+
+            const int Length = 2048;
+            var buffer = new byte[Length];
+            if (responseStream != null)
+            {
+                var bytesRead = responseStream.Read(buffer, 0, Length);
+
+                while (bytesRead > 0)
+                {
+                    writeStream.Write(buffer, 0, bytesRead);
+                    bytesRead = responseStream.Read(buffer, 0, Length);
+                }
+
+                responseStream.Close();
+            }
+
+            writeStream.Close();
+        }
+
+        #endregion FTP
     }
 }
