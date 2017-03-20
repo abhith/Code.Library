c34dd09f2e1ac580b03563ccd684339ec51c2d01
Abhith <dearabhith@gmail.com>
2017-03-21 01:44:33 +0400

    Succeeded result tests added

---
+++ Source/Code.Library/Code.Library.Tests/Models/SucceededResultTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Code.Library.Tests.Models
+{
+    using Library.Models;
+
+    using Shouldly;
+
+    using Xunit;
+
+    public class SucceededResultTests
+    {
+        [Fact]
+        public void Can_create_a_non_generic_version()
+        {
+            Result result = Result.Ok();
+
+            result.IsFailure.ShouldBe(false);
+            result.IsSuccess.ShouldBe(true);
+        }
+
+        [Fact]
+        public void Can_create_a_generic_version()
+        {
+            var myClass = new MyClass();
+
+            Result<MyClass> result = Result.Ok(myClass);
+
+            result.IsFailure.ShouldBe(false);
+            result.IsSuccess.ShouldBe(true);
+            result.Value.ShouldBe(myClass);
+        }
+
+        [Fact]
+        public void Cannot_create_without_Value()
+        {
+            Action action = () => { Result.Ok((MyClass)null); };
+
+            action.ShouldThrow<ArgumentNullException>(); ;
+        }
+
+        [Fact]
+        public void Cannot_access_Error_non_generic_version()
+        {
+            Result result = Result.Ok();
+
+            Action action = () =>
+            {
+                string error = result.Error;
+            };
+
+            action.ShouldThrow<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Cannot_access_Error_generic_version()
+        {
+            Result<MyClass> result = Result.Ok(new MyClass());
+
+            Action action = () =>
+            {
+                string error = result.Error;
+            };
+
+            action.ShouldThrow<InvalidOperationException>();
+        }
+
+        private class MyClass
+        {
+        }
+    }
+}
