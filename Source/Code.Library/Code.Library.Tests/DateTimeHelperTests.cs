43ba89cca7b8bce26e3bffe76867346a10d7a1e5
Abhith <dearabhith@gmail.com>
2017-03-21 01:01:09 +0400

    test commented

--- Source/Code.Library/Code.Library.Tests/DateTimeHelperTests.cs
+++ Source/Code.Library/Code.Library.Tests/DateTimeHelperTests.cs
@@ -1,23 +1,15 @@
-﻿using Code.Library;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace Code.Library.Tests
 {
-    [TestClass()]
     public class DateTimeHelperTests
     {
-        [TestMethod()]
+        //[Fact]
         public void GetArabianStandardTimeTest()
         {
             // Arrange
             var serverDate = DateTime.Now;
             var uaeTime = serverDate.GetArabianStandardTime();
-            Assert.AreNotEqual(serverDate, uaeTime);
         }
     }
 }
