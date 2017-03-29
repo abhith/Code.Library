34dcd7ca090018df080fb6381da08ee07766c68d
Abhith <dearabhith@gmail.com>
2017-03-30 00:38:55 +0400

    rather than returning exception while accessing value in case failure, default / null value returned to avoid error during serializing output

--- Source/Code.Library/Code.Library/Models/Result.cs
+++ Source/Code.Library/Code.Library/Models/Result.cs
@@ -11,60 +11,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Code.Library.Models
 {
     using System.Diagnostics;
 
-    internal sealed class ResultCommonLogic
-    {
-        public bool IsFailure { get; }
-        public bool IsSuccess => !IsFailure;
-
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private readonly string _error;
-
-        public string Error
-        {
-            [DebuggerStepThrough]
-            get
-            {
-                if (IsSuccess)
-                    throw new InvalidOperationException("There is no error message for success.");
-
-                return _error;
-            }
-        }
-
-        [DebuggerStepThrough]
-        public ResultCommonLogic(bool isFailure, string error)
-        {
-            if (isFailure)
-            {
-                if (string.IsNullOrEmpty(error))
-                    throw new ArgumentNullException(nameof(error), "There must be error message for failure.");
-            }
-            else
-            {
-                if (error != null)
-                    throw new ArgumentException("There should be no error message for success.", nameof(error));
-            }
-
-            IsFailure = isFailure;
-            _error = error;
-        }
-    }
-
     public struct Result
     {
+        #region Private Fields
+
         private static readonly Result OkResult = new Result(false, null);
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private readonly ResultCommonLogic _logic;
 
-        public bool IsFailure => _logic.IsFailure;
-        public bool IsSuccess => _logic.IsSuccess;
-        public string Error => _logic.Error;
+        #endregion Private Fields
+
+        #region Private Constructors
 
         [DebuggerStepThrough]
         private Result(bool isFailure, string error)
@@ -72,45 +37,17 @@ namespace Code.Library.Models
             _logic = new ResultCommonLogic(isFailure, error);
         }
 
-        [DebuggerStepThrough]
-        public static Result Ok()
-        {
-            return OkResult;
-        }
+        #endregion Private Constructors
 
-        [DebuggerStepThrough]
-        public static Result Fail(string error)
-        {
-            return new Result(true, error);
-        }
+        #region Public Properties
 
-        [DebuggerStepThrough]
-        public static Result<T> Ok<T>(T value)
-        {
-            return new Result<T>(false, value, null);
-        }
+        public string Error => _logic.Error;
+        public bool IsFailure => _logic.IsFailure;
+        public bool IsSuccess => _logic.IsSuccess;
 
-        [DebuggerStepThrough]
-        public static Result<T> Fail<T>(string error)
-        {
-            return new Result<T>(true, default(T), error);
-        }
+        #endregion Public Properties
 
-        /// <summary>
-        /// Returns first failure in the list of <paramref name="results"/>. If there is no failure returns success.
-        /// </summary>
-        /// <param name="results">List of results.</param>
-        [DebuggerStepThrough]
-        public static Result FirstFailureOrSuccess(params Result[] results)
-        {
-            foreach (Result result in results)
-            {
-                if (result.IsFailure)
-                    return Fail(result.Error);
-            }
-
-            return Ok();
-        }
+        #region Public Methods
 
         /// <summary>
         /// Returns failure which combined from all failures in the <paramref name="results"/> list. Error messages are separated by <paramref name="errorMessagesSeparator"/>.
@@ -148,31 +85,63 @@ namespace Code.Library.Models
             Result[] untyped = results.Select(result => (Result)result).ToArray();
             return Combine(errorMessagesSeparator, untyped);
         }
+
+        [DebuggerStepThrough]
+        public static Result Fail(string error)
+        {
+            return new Result(true, error);
+        }
+
+        [DebuggerStepThrough]
+        public static Result<T> Fail<T>(string error)
+        {
+            return new Result<T>(true, default(T), error);
+        }
+
+        /// <summary>
+        /// Returns first failure in the list of <paramref name="results"/>. If there is no failure returns success.
+        /// </summary>
+        /// <param name="results">List of results.</param>
+        [DebuggerStepThrough]
+        public static Result FirstFailureOrSuccess(params Result[] results)
+        {
+            foreach (Result result in results)
+            {
+                if (result.IsFailure)
+                    return Fail(result.Error);
+            }
+
+            return Ok();
+        }
+
+        [DebuggerStepThrough]
+        public static Result Ok()
+        {
+            return OkResult;
+        }
+
+        [DebuggerStepThrough]
+        public static Result<T> Ok<T>(T value)
+        {
+            return new Result<T>(false, value, null);
+        }
+
+        #endregion Public Methods
     }
 
     public struct Result<T>
     {
+        #region Private Fields
+
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private readonly ResultCommonLogic _logic;
-
-        public bool IsFailure => _logic.IsFailure;
-        public bool IsSuccess => _logic.IsSuccess;
-        public string Error => _logic.Error;
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private readonly T _value;
 
-        public T Value
-        {
-            [DebuggerStepThrough]
-            get
-            {
-                if (!IsSuccess)
-                    throw new InvalidOperationException("There is no value for failure.");
+        #endregion Private Fields
 
-                return _value;
-            }
-        }
+        #region Internal Constructors
 
         [DebuggerStepThrough]
         internal Result(bool isFailure, T value, string error)
@@ -184,6 +153,33 @@ namespace Code.Library.Models
             _value = value;
         }
 
+        #endregion Internal Constructors
+
+        #region Public Properties
+
+        public string Error => _logic.Error;
+        public bool IsFailure => _logic.IsFailure;
+        public bool IsSuccess => _logic.IsSuccess;
+
+        public T Value
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                if (!IsSuccess)
+                {
+                    //throw new InvalidOperationException("There is no value for failure.");
+                    return default(T);
+                }
+
+                return _value;
+            }
+        }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
         public static implicit operator Result(Result<T> result)
         {
             if (result.IsSuccess)
@@ -191,5 +187,59 @@ namespace Code.Library.Models
             else
                 return Result.Fail(result.Error);
         }
+
+        #endregion Public Methods
+    }
+
+    internal sealed class ResultCommonLogic
+    {
+        #region Private Fields
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly string _error;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        [DebuggerStepThrough]
+        public ResultCommonLogic(bool isFailure, string error)
+        {
+            if (isFailure)
+            {
+                if (string.IsNullOrEmpty(error))
+                    throw new ArgumentNullException(nameof(error), "There must be error message for failure.");
+            }
+            else
+            {
+                if (error != null)
+                    throw new ArgumentException("There should be no error message for success.", nameof(error));
+            }
+
+            IsFailure = isFailure;
+            _error = error;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public string Error
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                if (IsSuccess)
+                    //throw new InvalidOperationException("There is no error message for success.");
+                    return null;
+
+                return _error;
+            }
+        }
+
+        public bool IsFailure { get; }
+        public bool IsSuccess => !IsFailure;
+
+        #endregion Public Properties
     }
 }
