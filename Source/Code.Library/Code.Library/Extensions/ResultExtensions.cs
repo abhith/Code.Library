a76df96784e620ff9e0d668384957756a61c9e8e
Abhith <dearabhith@gmail.com>
2017-03-30 00:42:28 +0400

    reorganized code

--- Source/Code.Library/Code.Library/Extensions/ResultExtensions.cs
+++ Source/Code.Library/Code.Library/Extensions/ResultExtensions.cs
@@ -16,61 +16,7 @@ namespace Code.Library.Extensions
 
     public static class ResultExtensions
     {
-        public static Result<K> OnSuccess<T, K>(this Result<T> result, Func<T, K> func)
-        {
-            if (result.IsFailure)
-                return Result.Fail<K>(result.Error);
-
-            return Result.Ok(func(result.Value));
-        }
-
-        public static Result<T> OnSuccess<T>(this Result result, Func<T> func)
-        {
-            if (result.IsFailure)
-                return Result.Fail<T>(result.Error);
-
-            return Result.Ok(func());
-        }
-
-        public static Result<K> OnSuccess<T, K>(this Result<T> result, Func<T, Result<K>> func)
-        {
-            if (result.IsFailure)
-                return Result.Fail<K>(result.Error);
-
-            return func(result.Value);
-        }
-
-        public static Result<T> OnSuccess<T>(this Result result, Func<Result<T>> func)
-        {
-            if (result.IsFailure)
-                return Result.Fail<T>(result.Error);
-
-            return func();
-        }
-
-        public static Result<K> OnSuccess<T, K>(this Result<T> result, Func<Result<K>> func)
-        {
-            if (result.IsFailure)
-                return Result.Fail<K>(result.Error);
-
-            return func();
-        }
-
-        public static Result OnSuccess<T>(this Result<T> result, Func<T, Result> func)
-        {
-            if (result.IsFailure)
-                return Result.Fail(result.Error);
-
-            return func(result.Value);
-        }
-
-        public static Result OnSuccess(this Result result, Func<Result> func)
-        {
-            if (result.IsFailure)
-                return result;
-
-            return func();
-        }
+        #region Public Methods
 
         public static Result<T> Ensure<T>(this Result<T> result, Func<T, bool> predicate, string errorMessage)
         {
@@ -108,26 +54,6 @@ namespace Code.Library.Extensions
                 return Result.Fail<T>(result.Error);
 
             return Result.Ok(func());
-        }
-
-        public static Result<T> OnSuccess<T>(this Result<T> result, Action<T> action)
-        {
-            if (result.IsSuccess)
-            {
-                action(result.Value);
-            }
-
-            return result;
-        }
-
-        public static Result OnSuccess(this Result result, Action action)
-        {
-            if (result.IsSuccess)
-            {
-                action();
-            }
-
-            return result;
         }
 
         public static T OnBoth<T>(this Result result, Func<Result, T> func)
@@ -218,5 +144,83 @@ namespace Code.Library.Extensions
 
             return result;
         }
+
+        public static Result<K> OnSuccess<T, K>(this Result<T> result, Func<T, K> func)
+        {
+            if (result.IsFailure)
+                return Result.Fail<K>(result.Error);
+
+            return Result.Ok(func(result.Value));
+        }
+
+        public static Result<T> OnSuccess<T>(this Result result, Func<T> func)
+        {
+            if (result.IsFailure)
+                return Result.Fail<T>(result.Error);
+
+            return Result.Ok(func());
+        }
+
+        public static Result<K> OnSuccess<T, K>(this Result<T> result, Func<T, Result<K>> func)
+        {
+            if (result.IsFailure)
+                return Result.Fail<K>(result.Error);
+
+            return func(result.Value);
+        }
+
+        public static Result<T> OnSuccess<T>(this Result result, Func<Result<T>> func)
+        {
+            if (result.IsFailure)
+                return Result.Fail<T>(result.Error);
+
+            return func();
+        }
+
+        public static Result<K> OnSuccess<T, K>(this Result<T> result, Func<Result<K>> func)
+        {
+            if (result.IsFailure)
+                return Result.Fail<K>(result.Error);
+
+            return func();
+        }
+
+        public static Result OnSuccess<T>(this Result<T> result, Func<T, Result> func)
+        {
+            if (result.IsFailure)
+                return Result.Fail(result.Error);
+
+            return func(result.Value);
+        }
+
+        public static Result OnSuccess(this Result result, Func<Result> func)
+        {
+            if (result.IsFailure)
+                return result;
+
+            return func();
+        }
+
+        public static Result<T> OnSuccess<T>(this Result<T> result, Action<T> action)
+        {
+            if (result.IsSuccess)
+            {
+                action(result.Value);
+            }
+
+            return result;
+        }
+
+        public static Result OnSuccess(this Result result, Action action)
+        {
+            if (result.IsSuccess)
+            {
+                action();
+            }
+
+            return result;
+        }
+
+        #endregion Public Methods
     }
 }
