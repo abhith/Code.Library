3f0a224ba26f736dd34d54736789332f963ea57b
Abhith <abhith.rajan@unibetonrm.com>
2018-12-11 12:10:04 +0400

    ConvertTo12H method moved to DateTimeExtensions

--- src/Code.Library/Extensions/DateTimeExtensions.cs
+++ src/Code.Library/Extensions/DateTimeExtensions.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using System.Globalization;
-
-namespace Code.Library
+﻿namespace Code.Library
 {
-    public static class ConvertHelper
+    using System;
+    using System.Globalization;
+
+    /// <summary>
+    /// All Date Time extensions.
+    /// </summary>
+    public static class DateTimeExtensions
     {
         /// <summary>
         /// Convert timespan to 12H format string
         /// </summary>
-        /// <param name="timeSpan"></param>
-        /// <returns></returns>
+        /// <param name="timeSpan">Input timespan.</param>
+        /// <returns>Tweleve hour format string.</returns>
         public static string ConvertTo12H(this TimeSpan timeSpan)
         {
             var dateTime = DateTime.MinValue.Add(timeSpan);
             var cultureInfo = CultureInfo.InvariantCulture;
-
-            // optional
-            //CultureInfo cultureInfo = new CultureInfo(CultureInfo.CurrentCulture.Name);
-            //cultureInfo.DateTimeFormat.PMDesignator = "PM";
 
             return dateTime.ToString("hh:mm tt", cultureInfo);
         }
