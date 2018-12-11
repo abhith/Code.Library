c74f3d514738e8326049027d9c77d836eaa27182
Abhith <abhith.rajan@unibetonrm.com>
2018-12-11 11:27:38 +0400

    new IEnumerable Extensions class added

--- src/Code.Library/Helpers/ConvertHelper.cs
+++ src/Code.Library/Helpers/ConvertHelper.cs
@@ -1,35 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Globalization;
 
 namespace Code.Library
 {
     public static class ConvertHelper
     {
-        /// <summary>
-        /// IEnumerable to datatable
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        public static DataTable ToDataTable<T>(IEnumerable<T> data)
-        {
-            var properties = TypeDescriptor.GetProperties(typeof(T));
-            var table = new DataTable();
-            foreach (PropertyDescriptor prop in properties)
-                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
-            foreach (T item in data)
-            {
-                var row = table.NewRow();
-                foreach (PropertyDescriptor prop in properties)
-                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
-                table.Rows.Add(row);
-            }
-            return table;
-        }
-
         #region Extension Methods
 
         /// <summary>
