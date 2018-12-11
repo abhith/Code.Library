4ef6b4b7cba5440c07d3d3c9dd7b929bb6f85eaa
Abhith <dearabhith@gmail.com>
2018-12-11 16:59:09 +0400

    string helper methods moved to extensions

---
+++ src/Code.Library/Helpers/DateTimeHelper.cs
@@ -0,0 +1,48 @@
+﻿namespace Code.Library
+{
+    using System;
+
+    /// <summary>
+    /// All DateTime helper methods.
+    /// </summary>
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// Format Dates.
+        /// </summary>
+        /// <param name="startDate">Start Date.</param>
+        /// <param name="endDate">End Date.</param>
+        /// <returns>Formatted date string.</returns>
+        public static string FormatDates(DateTime? startDate, DateTime? endDate)
+        {
+            if (startDate != null && endDate != null)
+            {
+                if (startDate == endDate)
+                {
+                    return Convert.ToDateTime(startDate).ToString("MMM dd , yyyy");
+                }
+                else
+                {
+                    if (Convert.ToDateTime(startDate).ToString("MM yyyy") ==
+                        Convert.ToDateTime(endDate).ToString("MM yyyy"))
+                    {
+                        return Convert.ToDateTime(startDate).ToString("MMM dd") + " - " +
+                               Convert.ToDateTime(endDate).ToString("MMM dd , yyyy");
+                    }
+                    else
+                    {
+                        return Convert.ToDateTime(startDate).ToString("MMM dd , yyyy") + " - " +
+                               Convert.ToDateTime(endDate).ToString("MMM dd , yyyy");
+                    }
+                }
+            }
+
+            if (startDate != null)
+            {
+                return Convert.ToDateTime(startDate).ToString("MMM dd , yyyy");
+            }
+
+            return endDate != null ? Convert.ToDateTime(endDate).ToString("MMM dd , yyyy") : "not specified !";
+        }
+    }
+}
