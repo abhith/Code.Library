b09d5a782bfd6932a808c2f032e15513ae8fab86
Abhith <dearabhith@gmail.com>
2018-12-11 17:02:21 +0400

    Validation helper methods moved to StringExtensions

--- src/Code.Library/Extensions/StringExtensions.cs
+++ src/Code.Library/Extensions/StringExtensions.cs
@@ -161,9 +161,29 @@
             return true;
         }
 
+        /// <summary>
+        /// To check whether the given string is Arabic.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Returns True if Arabic</returns>
+        public static bool IsRtl(this string input)
+        {
+            return Regex.IsMatch(input, @"\p{IsArabic}");
+        }
+
         public static bool IsUpperCase(this char ch)
         {
             return ch.ToString(CultureInfo.InvariantCulture) == ch.ToString(CultureInfo.InvariantCulture).ToUpperInvariant();
+        }
+
+        /// <summary>
+        /// To check whether the string is a valid email.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public static bool IsValidEmailAddress(this string email)
+        {
+            return Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
         }
 
         /// <summary>
