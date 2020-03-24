8104c00fb94b2d046a0ae2c82e8c441904574aec
Abhith Rajan <dearabhith@gmail.com>
2020-03-24 11:09:35 +0400

    feat: add appinsights service collection extension

---
+++ src/Code.Library.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using AspNetMonsters.ApplicationInsights.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Code.Library.AspNetCore
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAppInsight(this IServiceCollection services, IConfiguration configuration, string cloudRoleName)
+        {
+            services
+                .AddApplicationInsightsTelemetry(configuration)
+                .AddCloudRoleNameInitializer(cloudRoleName);
+            return services;
+        }
+    }
+}
