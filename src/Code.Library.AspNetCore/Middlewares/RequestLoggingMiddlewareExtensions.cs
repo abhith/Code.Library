aca3a8668868f2c1bfc7267af5ce72f1d9088ba6
Abhith Rajan <dearabhith@gmail.com>
2020-03-25 02:16:01 +0400

    feat: add request logging middleware

---
+++ src/Code.Library.AspNetCore/Middlewares/RequestLoggingMiddlewareExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Serilog;
+
+namespace Code.Library.AspNetCore.Middlewares
+{
+    public static class RequestLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLogging(this IApplicationBuilder builder)
+        {
+            return builder
+                .UseSerilogRequestLogging(options =>
+                options.EnrichDiagnosticContext = SerilogHelpers.EnrichFromRequest);
+        }
+    }
+}
--- src/Code.Library.AspNetCore/SerilogHelpers.cs
+++ src/Code.Library.AspNetCore/SerilogHelpers.cs
@@ -1,36 +1,57 @@
 ﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Formatting.Compact;
-using System;
 using System.Reflection;
 
 namespace Code.Library.AspNetCore
 {
     public static class SerilogHelpers
     {
+        public static void EnrichFromRequest(
+                    IDiagnosticContext diagnosticContext, HttpContext httpContext)
+        {
+            var request = httpContext.Request;
+            diagnosticContext.Set("Host", request.Host);
+            diagnosticContext.Set("Protocol", request.Protocol);
+            diagnosticContext.Set("Scheme", request.Scheme);
+            if (request.QueryString.HasValue)
+            {
+                diagnosticContext.Set("QueryString", request.QueryString.Value);
+            }
+
+            diagnosticContext.Set("ContentType", httpContext.Response.ContentType);
+
+            diagnosticContext.Set("UserAgent", httpContext.Request.Headers["User-Agent"].ToString());
+            diagnosticContext.Set("ClientIP", httpContext.Connection.RemoteIpAddress);
+            diagnosticContext.Set("UserName", httpContext.User.Identity.Name == null ? "(anonymous)" : httpContext.User.Identity.Name);
+
+            //var endpoint = httpContext.GetEndpoint();
+            //if (endpoint is object)
+            //{
+            //    diagnosticContext.Set("EndpointName", endpoint.DisplayName);
+            //}
+        }
+
         /// <summary>
         /// Provides standardized, centralized Serilog wire-up for a suite of applications.
         /// </summary>
         /// <param name="loggerConfig">Provide this value from the UseSerilog method param</param>
-        /// <param name="provider">Provide this value from the UseSerilog method param as well</param>
-        /// <param name="applicationName">Represents the name of YOUR APPLICATION and will be used to segregate your app
-        /// from others in the logging sink(s).</param>
         /// <param name="config">IConfiguration settings -- generally read this from appsettings.json</param>
         public static void WithSimpleConfiguration(this LoggerConfiguration loggerConfig,
-            IServiceProvider provider, string applicationName, IConfiguration config)
+              IConfiguration config)
         {
             var name = Assembly.GetEntryAssembly().GetName();
 
             loggerConfig
                 .ReadFrom.Configuration(config) // minimum levels defined per project in json files
-                                                //.Enrich.WithAspnetcoreHttpcontext(provider, AddCustomContextDetails)
                 .Enrich.FromLogContext()
                 .Enrich.WithMachineName()
                 .Enrich.WithProperty("Assembly", $"{name.Name}")
                 .Enrich.WithProperty("Version", $"{name.Version}")
                 .WriteTo.File(new CompactJsonFormatter(),
-                    @"Logs\log.json", rollingInterval: RollingInterval.Day)
+                    @"Logs\log.clef", rollingInterval: RollingInterval.Day)
                 //.WriteTo.File(@"Logs\log.txt", rollingInterval: RollingInterval.Day)
                 .WriteTo.ApplicationInsights(TelemetryConfiguration.Active, TelemetryConverter.Traces);
         }
