a3b109a88532007dfdc9e2e7e6126ec342eb03ba
Abhith <dearabhith@gmail.com>
2020-04-01 20:52:49 +0400

    refactor: mstests to xunit + fluent assertions

---
+++ src/Code.Library.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using Code.Library.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace Code.Library.Tests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        /// <summary>
+        /// The clean content test.
+        /// </summary>
+        [Fact]
+        public void CleanTest()
+        {
+            var input = "$7.42 billion";
+
+            var output = input.Clean(true);
+
+            output.Should().Be(input);
+
+            input = "SELECT accountNumber, balance FROM accounts WHERE account_owner_id = 0 OR 1=1";
+
+            output = input.Clean(true);
+
+            output.Should().NotBe(input);
+        }
+
+        [Fact]
+        public void IsNumberOnlyTest()
+        {
+            "12345".IsNumberOnly(false).Should().BeTrue();
+            "   12345".IsNumberOnly(false).Should().BeTrue();
+            "12.345".IsNumberOnly(true).Should().BeTrue();
+            "   12,345 ".IsNumberOnly(true).Should().BeTrue();
+            "12 345".IsNumberOnly(false).Should().BeTrue();
+            "tractor".IsNumberOnly(true).Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsNumberTest()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+            "12345".IsNumber(false).Should().BeTrue();
+            "   12345".IsNumber(false).Should().BeTrue();
+            "12.345".IsNumber(true).Should().BeTrue();
+            "   12,345 ".IsNumber(true).Should().BeTrue();
+            "12 345".IsNumber(false).Should().BeTrue();
+            "tractor".IsNumber(true).Should().BeFalse();
+        }
+
+        /// <summary>
+        ///A test for IsValidUrl
+        ///</summary>
+        [Fact]
+        public void IsValidUrlTest()
+        {
+            "http://www.codeproject.com".IsValidUrl().Should().BeTrue();
+            "https://www.codeproject.com/#some_anchor".IsValidUrl().Should().BeTrue();
+            "https://localhost".IsValidUrl().Should().BeTrue();
+            "http://www.abcde.nf.net/signs-banners.jpg".IsValidUrl().Should().BeTrue();
+            "http://aa-bbbb.cc.bla.com:80800/test/test/test.aspx?dd=dd&id=dki".IsValidUrl().Should().BeTrue();
+            "http:wwwcodeprojectcom".IsValidUrl().Should().BeFalse();
+            "http://www.code project.com".IsValidUrl().Should().BeFalse();
+        }
+
+        [Fact]
+        public void MD5Test()
+        {
+            string input = "The quick brown fox jumps over the lazy dog";
+            string expected = "9e107d9d372bb6826bd81d3542a419d6";
+            string actual = input.MD5();
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Nl2BrTest()
+        {
+            string input = "yellow dog" + Environment.NewLine + "black cat";
+            string expected = "yellow dog<br />black cat";
+            string actual = input.Nl2Br();
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        /// <summary>
+        ///A test for RemoveDiacritics
+        ///</summary>
+        [Fact]
+        public void RemoveDiacriticsTest()
+        {
+            //contains all czech accents
+            var input = "Příliš žluťoučký kůň úpěl ďábelské ódy.";
+            var expected = "Prilis zlutoucky kun upel dabelske ody.";
+            string actual = input.RemoveDiacritics();
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void RemoveSpacesTest()
+        {
+            string input = "yellow dog" + Environment.NewLine + "black cat";
+            string expected = "yellowdog" + Environment.NewLine + "blackcat";
+            string actual = input.RemoveSpaces();
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ReverseTest()
+        {
+            string input = "yellow dog";
+            string expected = "god wolley";
+            string actual = input.Reverse();
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        /// <summary>
+        /// The to friendly url test.
+        /// </summary>
+        [Fact]
+        public void ToFriendlyUrlTest()
+        {
+            var input = "New Text Document";
+            var expected = "new-text-document";
+            input.ToFriendlyUrl().Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void UrlAvailableTest()
+        {
+            "www.codeproject.com".UrlAvailable().Should().BeTrue();
+            "www.asjdfalskdfjalskdf.com".UrlAvailable().Should().BeFalse();
+        }
+    }
+}
