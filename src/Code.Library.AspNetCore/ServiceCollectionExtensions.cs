4c6f5341120bf41bf486c6d990b3e4f6ae39bfa2
Abhith Rajan <dearabhith@gmail.com>
2020-04-01 18:22:40 +0400

    feat: add flurl request logging module

--- src/Code.Library.AspNetCore/ServiceCollectionExtensions.cs
+++ src/Code.Library.AspNetCore/ServiceCollectionExtensions.cs
@@ -1,7 +1,10 @@
 ﻿using AspNetMonsters.ApplicationInsights.AspNetCore;
+using Flurl.Http;
 using Hellang.Middleware.ProblemDetails;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System.Threading.Tasks;
 
 namespace Code.Library.AspNetCore
 {
@@ -19,6 +22,32 @@ namespace Code.Library.AspNetCore
                 .AddApplicationInsightsTelemetry(configuration)
                 .AddCloudRoleNameInitializer(cloudRoleName);
             return services;
+        }
+
+        public static IServiceCollection AddFlurlTelemetry(this IServiceCollection services)
+        {
+            FlurlHttp.Configure((settings) => settings.AfterCallAsync = LogFlurlCallsAsync);
+            return services;
+        }
+
+        private static async Task LogFlurlCallsAsync(HttpCall call)
+        {
+            var responseBody = string.Empty;
+
+            if (call.Response != null &&
+                call.Response.Content != null)
+            {
+                var ct = call.Response.Content.Headers.ContentType.ToString();
+                // avoid logging file downloads
+                if (ct.Contains("application/json") ||
+                    ct.Contains("text/plain"))
+                {
+                    responseBody = await call.Response.Content.ReadAsStringAsync();
+                }
+            }
+
+            Log.Information("External Request | {Endpoint} {Succeeded} {RequestBody} {HttpStatusCode} {ResponseBody}", call.ToString(), call.Succeeded, call.RequestBody, call.HttpStatus.HasValue ?
+                    (int)call.HttpStatus : default(int?), responseBody);
         }
     }
 }
