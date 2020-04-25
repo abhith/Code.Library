d9febe3a2c41847b1d3f3cb50d0ab2bca43d04c9
Abhith Rajan <dearabhith@gmail.com>
2020-04-25 17:59:49 +0400

    feat: add MapDefaultHealthChecks extension

---
+++ src/Code.Library.AspNetCore/Extensions/HealthCheckEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using HealthChecks.UI.Client;
+
+namespace Code.Library.AspNetCore.Extensions
+{
+    public static class HealthCheckEndpointRouteBuilderExtensions
+    {
+        public static IEndpointRouteBuilder MapDefaultHealthChecks(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+            endpoints.MapHealthChecks("/liveness", new HealthCheckOptions
+            {
+                Predicate = r => r.Name.Contains("self")
+            });
+
+            return endpoints;
+        }
+    }
+}
