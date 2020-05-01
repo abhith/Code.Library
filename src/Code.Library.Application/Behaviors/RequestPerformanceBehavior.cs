cef05c2ba02b7d95a2454d98b658d0fddbc99780
Abhith Rajan <dearabhith@gmail.com>
2020-05-01 14:43:01 +0400

    feat: add request performance

---
+++ src/Code.Library.Application/Behaviors/RequestPerformanceBehavior.cs
@@ -0,0 +1,49 @@
+﻿using Code.Library.Extensions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Code.Library.Application.Behaviors
+{
+    /// <summary>
+    /// This will add an additional log for requests that taking more than the threshold (for now, it is set to 500ms)
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <remarks>Credits: https://github.com/jasontaylordev/CleanArchitecture</remarks>
+    public class RequestPerformanceBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+        private readonly Stopwatch _timer;
+
+        public RequestPerformanceBehavior(
+            ILogger<TRequest> logger)
+        {
+            _timer = new Stopwatch();
+
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+            // TODO(abhith): remove hard coded threshold
+            if (elapsedMilliseconds > 500)
+            {
+                _logger.LogWarning("Long Running Request: {RequestName} ({ElapsedMilliseconds} milliseconds) {@Request}",
+                    request.GetGenericTypeName(), elapsedMilliseconds, request);
+            }
+
+            return response;
+        }
+    }
+}
