12a79851305031274b0f4f48bcb8f7f54c0ed322
Abhith Rajan <dearabhith@gmail.com>
2020-05-01 14:43:33 +0400

    feat: add unhandled exception behaviors

---
+++ src/Code.Library.Application/Behaviors/UnhandledExceptionBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Code.Library.Domain.Exceptions;
+using Code.Library.Extensions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Code.Library.Application.Behaviors
+{
+    /// <summary>
+    /// For any exception that thrown in the request pipeline, except `DomainException`s, by adding this behavior, it will log the exception.
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <remarks>Credits: https://github.com/jasontaylordev/CleanArchitecture</remarks>
+    public class UnhandledExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehavior(ILogger<TRequest> logger) => _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (DomainException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled Exception for Request {Name} {@Request}", request.GetGenericTypeName(), request);
+
+                throw;
+            }
+        }
+    }
+}
