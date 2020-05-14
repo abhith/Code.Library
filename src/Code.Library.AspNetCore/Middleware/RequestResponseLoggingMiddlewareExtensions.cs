69580fdf4a083b9e9ccb9c553cfee1960d577e42
Abhith Rajan <dearabhith@gmail.com>
2020-05-14 18:24:38 +0400

    feat: add request response logging middleware

---
+++ src/Code.Library.AspNetCore/Middleware/RequestResponseLoggingMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Code.Library.AspNetCore.Middleware
+{
+    public static class RequestResponseLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestResponseLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestResponseLoggingMiddleware>();
+        }
+    }
+}
