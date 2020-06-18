a38d93c053f3784418ed611823f03971baec9ce9
Abhith Rajan <dearabhith@gmail.com>
2020-06-18 08:45:24 +0400

    chore(sample): sensitive content

--- Samples/AspNetCoreApp/Controllers/WeatherForecastController.cs
+++ Samples/AspNetCoreApp/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -26,6 +27,21 @@ namespace AspNetCoreApp.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post(SensitiveContent input)
+        {
+            _logger.LogInformation("Posted {@SensitiveContent}", input);
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
---
+++ Samples/AspNetCoreApp/Models/SensitiveContent.cs
@@ -0,0 +1,14 @@
+﻿using Destructurama.Attributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreApp.Models
+{
+    public class SensitiveContent
+    {
+        [LogMasked]
+        public string Password { get; set; }
+    }
+}
