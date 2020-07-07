d601a197384741f49af5b0bcc92edbfb4c3d30a3
Abhith Rajan <dearabhith@gmail.com>
2020-07-07 11:29:31 +0400

    chore(sample): validation exception
    WIP

---
+++ Samples/AspNetCoreApp.Application/ApplicationLogic.cs
@@ -0,0 +1,12 @@
+﻿using Code.Library.Application.Exceptions;
+
+namespace AspNetCoreApp.Application
+{
+    public class ApplicationLogic
+    {
+        public void GenerateValidationError()
+        {
+            throw new ValidationException();
+        }
+    }
+}
